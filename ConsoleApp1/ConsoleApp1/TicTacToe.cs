eb82422e2aae041adf6558ec656871a76e8a71df
Tyler <tug07294@temple.edu>
2019-02-19 11:57:18 -0500

    bTest.txt Committed

---
+++ ConsoleApp1/ConsoleApp1/TicTacToe.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    public class TicTacToe
+    {
+
+        public string[] board = new string[9];
+        
+
+        public TicTacToe()
+        {
+          
+        }
+
+        //string array is imagined to number the board going like  1|2|3
+        //                                                         4|5|6
+        //                                                         7|8|9 
+
+
+
+        public string[] CreateBoard()
+        {
+
+            return board;
+        }
+
+
+
+        public string CurrentPlayer(int count)
+        {
+            string s = "";
+
+            if (count == 1)
+            {
+                s = "it is player 1's turn. You will be 'X'";
+            }
+            if(count ==2)
+            {
+                s = "it is player 2's turn. you will be 'O'";
+            }
+            return s;
+
+        }
+
+
+
+        public string[] MakeMove(int count, int space)
+        {
+            if (count == 1)
+            {
+                board[space] = "x";
+            }
+            if (count == 2)
+            {
+                board[space] = "o";
+            }
+
+            return board;
+
+        }
+
+        public void ChangePlayer(int count)
+        {
+            if (count == 1)
+            {
+                count++;
+            }
+            if (count == 2)
+            {
+                count--;
+            }
+        }
+
+        public string SpaceInUse(int space)
+        {
+            string s = "";
+           
+            
+            
+                
+                if (board[space] == "" )
+                {
+                    s = "this space is open";
+                }
+            
+
+            return s;
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/TicTacToeGameTests/TicTacToeTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToeGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame.Tests
+{
+    [TestClass()]
+    public class TicTacToeTests
+    {
+       
+        //create board tests
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            int expected = 9; 
+            TicTacToe test = new TicTacToe();
+            int actual = test.CreateBoard().Length;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        //current player tests. 
+        [TestMethod()]
+        public void CurrentPlayerXTest()
+        {
+
+            string expected = "it is player 1's turn. You will be 'X'";
+            TicTacToe x = new TicTacToe();
+            string actual= x.CurrentPlayer(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerOTest()
+        {
+
+            string expected ="it is player 2's turn. you will be 'O'";
+            TicTacToe o = new TicTacToe();
+            string actual = o.CurrentPlayer(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //tests that players make move on the board
+        [TestMethod()]
+        public void XMakeMoveTest()
+        {
+            string expected = "x";
+            TicTacToe move = new TicTacToe();
+            string [] actualArray = move.MakeMove(1, 0);
+            string actual = actualArray[0];
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OMakeMoveTest()
+        {
+            string expected =  "o";
+            TicTacToe move = new TicTacToe();
+            string[] actualArray = move.MakeMove(2, 1);
+            string actual = actualArray[1];
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+
+        //tests that it is an unoccupied spot
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            
+            string expected = "this space is open";
+            TicTacToe free = new TicTacToe();
+            string actual = free.SpaceInUse(4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
